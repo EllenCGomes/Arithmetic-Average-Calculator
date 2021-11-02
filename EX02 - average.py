db64bbe9d115dcef5018374cd0c223bb8759841d
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2021-11-02 16:31:16 -0300

    Add files via upload

---
+++ EX02 - average.py
@@ -0,0 +1,7 @@
+grade1 = float(input("Enter the first grade: "))
+grade2 = float(input("Enter the second grade: "))
+grade3 = float(input("Enter the third grade: "))
+grade4 = float(input("Enter the fourth grade: "))
+
+average = (grade1 + grade2 + grade3 + grade4)/4
+print("The grade average is", average)
